1b474312c0a36e3646d20d647c9fa8000b321062
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-13 21:13:54 +0100

    Trying out TDD first time properly in my life, started with an unit test

---
+++ TaskCat.Automaton/TaskCat.Automaton.Tests/TestFiniteStateMachine.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskCat.Automaton.Tests
+{
+    [TestFixture]
+    public class TestFiniteStateMachine
+    {
+        [Test]
+        public void TestCreation()
+        {
+            FiniteStateMachine machine = new FiniteStateMachine();
+            Assert.IsNotNull(machine);
+        }
+    }
+}
