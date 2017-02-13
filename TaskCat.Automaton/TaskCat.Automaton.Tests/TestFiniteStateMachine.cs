53e08b2a2a475066881a0cafd4dc74295125261e
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-13 21:45:58 +0100

    Added another simple unit test, making sure we can load FSM from a file

--- TaskCat.Automaton/TaskCat.Automaton.Tests/TestFiniteStateMachine.cs
+++ TaskCat.Automaton/TaskCat.Automaton.Tests/TestFiniteStateMachine.cs
@@ -1,20 +1,32 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace TaskCat.Automaton.Tests
 {
     [TestFixture]
     public class TestFiniteStateMachine
     {
+        public string ExecutingPath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
         [Test]
         public void TestCreation()
         {
-            FiniteStateMachine machine = new FiniteStateMachine();
+            FiniteStateMachine machine = new FiniteStateMachine("SampleMachine");
             Assert.IsNotNull(machine);
+            Assert.AreEqual("SampleMachine", machine.Name);
+        }
+
+        [Test]
+        public void TestCreation_NotNull()
+        {
+            var jsonFsm = File.ReadAllText($"{ExecutingPath}\\SampleFSM.json");
+            Assert.IsNotNull(jsonFsm);
+
+            var machine = FiniteStateMachine.FromJson(jsonFsm);
+            Assert.IsNotNull(machine);
+            Assert.AreEqual("ClassifiedDelivery", machine.Name);
+            Assert.AreEqual("default", machine.Variant);
         }
     }
 }
--- TaskCat.Automaton/TaskCat.Automaton/FiniteStateMachine.cs
+++ TaskCat.Automaton/TaskCat.Automaton/FiniteStateMachine.cs
@@ -1,13 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TaskCat.Automaton
+﻿namespace TaskCat.Automaton
 {
+    using Newtonsoft.Json;
+    using System;
+    using System.IO;
+
     public class FiniteStateMachine
     {
+        /// <summary>
+        /// Name of the finite state machine
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
 
+        /// <summary>
+        /// Possible variant of the finite state machine.
+        /// This is for having different variants of a single finite machine
+        /// </summary>
+        [JsonProperty("variant")]
+        public string Variant { get; set; } = "default";
+
+        public FiniteStateMachine(string name)
+        {
+            this.Name = name;
+        }
+
+        public static FiniteStateMachine FromJson(string json)
+        {
+            if (string.IsNullOrWhiteSpace(json))
+                throw new ArgumentException(nameof(json));
+
+            var fsm = JsonConvert.DeserializeObject<FiniteStateMachine>(json);
+            if (fsm == null)
+                throw new NullReferenceException(nameof(fsm));
+
+            return fsm;
+        }
+
+        public static FiniteStateMachine FromFile(string filePath)
+        {
+            using (StreamReader file = File.OpenText(filePath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                var fsm = serializer.Deserialize(file, typeof(FiniteStateMachine)) as FiniteStateMachine;
+                if (fsm == null)
+                    throw new NullReferenceException(nameof(fsm));
+
+                return fsm;
+            }
+        }
     }
 }
