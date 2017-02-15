4d04b09084087f4a06f9fa3855726114245f5aff
Swagata Prateek <thehoneymad@hotmail.com>
2017-02-15 22:58:34 +0100

    Added some more documentation

--- TaskCat.Automaton/TaskCat.Automaton/EventDefinition.cs
+++ TaskCat.Automaton/TaskCat.Automaton/EventDefinition.cs
@@ -1,10 +1,21 @@
-﻿using Marvin.JsonPatch.Operations;
-
-namespace TaskCat.Automaton
+﻿namespace TaskCat.Automaton
 {
+    using Marvin.JsonPatch.Operations;
+
+    /// <summary>
+    /// JSONPatch event to be sent as a message/event to 
+    /// the finite state machine
+    /// </summary>
     public class EventDefinition
     {
+        /// <summary>
+        /// Event/Message to be invoked from type
+        /// </summary>
         public string FromType { get; set; }
+
+        /// <summary>
+        /// JSONPatch operation to act on the node
+        /// </summary>
         public Operation Operation { get; set; }
     }
 }
