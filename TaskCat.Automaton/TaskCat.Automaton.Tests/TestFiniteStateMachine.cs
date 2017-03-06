37500d27b1486cb74691946f663507d444ebab53
Swagata Prateek <thehoneymad@hotmail.com>
2017-03-06 19:58:10 +0100

    GFETCH-394: Added machine state saving unit test

--- TaskCat.Automaton/TaskCat.Automaton.Tests/TestFiniteStateMachine.cs
+++ TaskCat.Automaton/TaskCat.Automaton.Tests/TestFiniteStateMachine.cs
@@ -68,5 +68,47 @@ namespace TaskCat.Automaton.Tests
             Assert.That(machine.CurrentCandidateNodes.Count() == 1);
             Assert.That(machine.CurrentCandidateNodes.Last().Type == "Delivery");
         }
+
+        [Test]
+        public void TestSaving_NotNull()
+        {
+            var jsonFsm = File.ReadAllText($"{ExecutingPath}\\SampleFSM.json");
+            Assert.IsNotNull(jsonFsm);
+
+            var machine = FiniteStateMachine.FromJson(jsonFsm);
+            Assert.IsNotNull(machine);
+
+            machine.Initiate();
+
+            Operation op = new Operation()
+            {
+                op = "replace",
+                path = "/state",
+                value = "COMPLETED"
+            };
+
+            var currentCandidates = machine.CurrentCandidateNodes;
+
+            Assert.IsNotNull(currentCandidates);
+            Assert.AreEqual(1, currentCandidates.Count());
+
+            var candidate = currentCandidates.First();
+
+            var eventPossibilities = machine.GetEvents(candidate.Id);
+            Assert.IsNotNull(eventPossibilities);
+            Assert.That(eventPossibilities.Count() >= 1);
+
+            machine.ExecuteEvent(new EventDefinition()
+            {
+                Id = eventPossibilities.First().Id,
+                NodeId = candidate.Id,
+                MatchCondition = op
+            });
+
+            Assert.That(machine.NodeHistory.Count == 1);
+
+            var save = machine.SaveMachine();
+            Assert.IsNotNull(save);
+        }
     }
 }
